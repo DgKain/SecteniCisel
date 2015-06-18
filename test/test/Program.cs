c7756393f08c3047c7ac4de300286c1bfd4978e0
Ondřej Kyzek <ondrej.kyzek@gmail.com>
2015-06-18 20:29:01 +0200

    zakladni funkce programu

---
+++ test/test/Program.cs
@@ -0,0 +1,33 @@
+﻿//Tohle je aplikace urcena pro testovani mych myslenek a hypotez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadejte počet čísel od 1 do 100, která chcete sečíst: ");
+            int pocetCisel;
+            int.TryParse(Console.ReadLine(), out pocetCisel);
+            
+            int vysledek = 0;
+
+            if ((pocetCisel >= 1) && (pocetCisel <= 100))
+            {
+                for (int i = 0; i < pocetCisel; i++)
+                {
+                    vysledek += (i + 1);
+                }
+                Console.WriteLine(vysledek);
+            }
+            else
+                Console.WriteLine("Nezadal jste hodnotu od 1 do 100");
+        }
+    }
+}
