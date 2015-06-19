2ed445f9db0253a8dc912d638d8e50b442616ad6
Ondřej Kyzek <ondrej.kyzek@gmail.com>
2015-06-19 20:55:36 +0200

    main program

---
+++ SecteniCisel/SecteniCisel/Pocty.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: kyzek
+ * Date: 19.6.2015
+ * Time: 13:57
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace SecteniCisel
+{
+	/// <summary>
+	/// Tato trida ma na starost veskere vypocty s cisly
+	/// </summary>
+	public class Pocty
+	{
+		public int sectiCisla(int pocetCisel)
+		{
+			int soucetCisel = 0;
+			
+			for (int i = 0; i < pocetCisel; i++)
+			{
+				soucetCisel += (i + 1);
+			}
+			
+			return soucetCisel;
+		}
+	}
+}
---
+++ SecteniCisel/SecteniCisel/Program.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: kyzek
+ * Date: 19.6.2015
+ * Time: 12:59
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace SecteniCisel
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Vítejte v programu pro sečtení prvních X čísel od 1 do 100!");
+			Console.WriteLine("Program spustíte libovolnou klávesou...");
+			Console.ReadKey();
+			
+			Pocty vypocet = new Pocty();
+			
+			int pocetCisel;
+			bool opakovat = true;
+			bool vybranaSpravnaVolba = false;
+			
+			while (opakovat)
+			{
+				opakovat = true;
+				vybranaSpravnaVolba = false;
+				
+				Console.Clear();
+				Console.Write("Zadejte prosím počet čísel od 1 do 100, které chcete sečíst: ");
+				
+				try
+				{
+					pocetCisel = int.Parse(Console.ReadLine());
+					
+					if ((pocetCisel >= 1) && (pocetCisel <= 100))
+					{
+						Console.WriteLine("Soucet prvnich {0} cisel je: {1}", pocetCisel, vypocet.sectiCisla(pocetCisel));
+						Console.ReadKey();
+					}
+					else
+					{
+						Console.WriteLine("Nezadali jste číslo v platném rozsahu od 1 do 100!");
+						Console.ReadKey();
+					}
+				}
+				
+				catch (Exception ex)
+				{
+					Console.WriteLine("Nezadali jste číslo v platném rozsahu od 1 do 100!");
+					Console.WriteLine();
+					Console.WriteLine(ex);
+					Console.WriteLine();
+					Console.WriteLine("Pokračujte libovolnou klávesou...");
+					Console.ReadKey();
+				}
+				
+				while (!vybranaSpravnaVolba)
+				{
+					Console.Clear();
+					Console.WriteLine("Zvolte jednu z možností:");
+					Console.WriteLine("1 - Opakovat zadání");
+					Console.WriteLine("2 - Ukončit program");
+					
+					char volba = Console.ReadKey().KeyChar;
+					
+					switch (volba)
+					{
+						case '1':
+							opakovat = true;
+							vybranaSpravnaVolba = true;
+							break;
+						case '2':
+							opakovat = false;
+							vybranaSpravnaVolba = true;
+							break;
+						default:
+							vybranaSpravnaVolba = false;
+							break;
+					}
+				}
+			}
+			
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("Libovolnou klávesou ukončíte program...");
+			Console.ReadKey();
+			
+		}
+	}
+}
